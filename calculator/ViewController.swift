5498d918fc019bc35109f5245093a82b00efb85a
kennansalisbury <kennansalisbury@gmail.com>
2020-07-01 17:46:33 -0700

    Initial Commit

---
+++ calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Kennan Salisbury on 7/1/20.
+//  Copyright © 2020 Kennan Salisbury. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
